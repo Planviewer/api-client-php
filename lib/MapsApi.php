472c84fe6fd2a0399d290f19a2ad845a77acec8e
Axel Simons <a.simons@planviewer.nl>
2019-12-03 15:23:38 +0100

    file-level docblock with license in library file

--- lib/MapsApi.php
+++ lib/MapsApi.php
@@ -1,5 +1,16 @@
 <?php
 
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2019 Planviewer BV.
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ *
+ * @see https://github.com/Planviewer/api-client-php
+ */
+
 namespace Planviewer;
 
 class MapsApi {
