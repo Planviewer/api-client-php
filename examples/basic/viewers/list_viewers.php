e9c9997495eeb9e7150c1a69d35c0327a0b47f72
Axel Simons <a.simons@planviewer.nl>
2019-12-03 14:14:22 +0100

    composer.json + autoload + bootstrap + voorbeeld-example-script

---
+++ examples/basic/viewers/list_viewers.php
@@ -0,0 +1,9 @@
+<?php
+
+require dirname(__DIR__).'/../bootstrap.php';
+
+use Planviewer\MapsApi;
+
+$mapsapi = new MapsApi();
+
+var_dump($mapsapi);
---
+++ lib/MapsApi.php
@@ -0,0 +1,6 @@
+<?php
+
+namespace Planviewer;
+
+class MapsApi {
+}
