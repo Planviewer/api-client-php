93acc3d7747b5630c76a2c448fbd8b80ad2ed773
Axel Simons <a.simons@planviewer.nl>
2019-12-03 15:26:31 +0100

    voorbeeld list-viewers

--- examples/basic/viewers/list_viewers.php
+++ examples/basic/viewers/list_viewers.php
@@ -1,9 +1,14 @@
 <?php
 
-require dirname(__DIR__).'/../bootstrap.php';
+$loader = require dirname(__DIR__).'/../bootstrap.php';
+$config = require dirname(__DIR__).'/../config.php';
+
 
 use Planviewer\MapsApi;
 
-$mapsapi = new MapsApi();
+$mapsapi = new MapsApi([
+    'auth' => [$config['api-key'], $config['api-secret']],
+    'base_uri' => (isset($config['base_uri']) ? $config['base_uri'] : 'https://www.planviewer.nl'),
+]);
 
-var_dump($mapsapi);
+var_dump($mapsapi->listViewers());
--- lib/MapsApi.php
+++ lib/MapsApi.php
@@ -13,5 +13,49 @@ declare(strict_types=1);
 
 namespace Planviewer;
 
-class MapsApi {
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Response;
+
+class MapsApi extends Client {
+
+    public function __construct(array $config = []) {
+
+        if(!isset($config['base_uri'])) {
+            $config['base_uri'] = 'https://www.planviewer.nl';
+        }
+
+        parent::__construct($config);
+    }
+
+    public function listViewers($limit = 10, $offset = 0) {
+
+        $viewers = [];
+        do {
+            $response = $this->request('GET', '/maps_api/v2/server/viewers', [
+                'query' => [
+                    'limit' => $limit,
+                    'offset' => $offset,
+                ],
+            ]);
+
+            $batch = $this->json_decode($response)->viewers;
+            $viewers = array_merge($viewers, $batch);
+            $offset += $limit;
+
+        } while(count($batch));
+
+        return $viewers;
+    }
+
+    public function json_decode(Response $response, $assoc = false, $depth = 512, $options = 0) {
+
+        $data = \json_decode((string) $response->getBody(), $assoc, $depth, $options);
+        if (JSON_ERROR_NONE !== json_last_error()) {
+            throw new \InvalidArgumentException(
+                'json_decode error: ' . json_last_error_msg()
+            );
+        }
+
+        return $data;
+    }
 }
