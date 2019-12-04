199ea864fcec4e1fd2f8ccf36f99a9f822f23ef2
Axel Simons <a.simons@planviewer.nl>
2019-12-04 13:36:07 +0100

    find autoload when installed as package

--- examples/bootstrap.php
+++ examples/bootstrap.php
@@ -1,4 +1,18 @@
 <?php
 
-require dirname(__DIR__).'/vendor/autoload.php';
+declare(strict_types=1);
+
+$paths = [
+     // local dev repository
+     __DIR__ . '/../vendor/autoload.php',
+     // dependency
+     __DIR__ . '/../../../autoload.php',
+];
+
+foreach ($paths as $path) {
+    if (file_exists($path)) {
+        require_once $path;
+        break;
+    }
+}
 
