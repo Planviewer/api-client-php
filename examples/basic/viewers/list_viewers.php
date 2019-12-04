f54c2969b9cf071cdec5e36445e91e0be20d3a01
Axel Simons <a.simons@planviewer.nl>
2019-12-04 08:54:24 +0100

    docblock met link naar api docs in example

--- examples/basic/viewers/list_viewers.php
+++ examples/basic/viewers/list_viewers.php
@@ -1,8 +1,21 @@
 <?php
 
+/**
+ * Copyright (c) 2019 Planviewer BV.
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ *
+ * @see https://github.com/Planviewer/api-client-php
+ *
+ *
+ * Example listing viewers in an Application
+ *
+ * @see https://docs.planviewer.nl/mapsapi/server_calls/viewers.html#list-viewers
+ */
+
 $loader = require dirname(__DIR__).'/../bootstrap.php';
 $config = require dirname(__DIR__).'/../config.php';
-
 
 use Planviewer\MapsApi;
 
